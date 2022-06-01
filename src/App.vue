0f4256b6b82b67b51fed9e6138f9aed5dea976af
JuaniSilva <juanicruzsilva@gmail.com>
2022-06-01 20:56:09 -0300

    fix: router push now work properly :D

--- src/App.vue
+++ src/App.vue
@@ -3,44 +3,44 @@ import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
-    <NavBar />
-    <router-view />
+	<NavBar />
+	<router-view :key="$route.path" />
 </template>
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;800&display=swap');
 html {
-    overflow: hidden !important;
+	overflow: hidden !important;
 }
 body {
-    margin: 0;
-    font-family: 'Montserrat', sans-serif;
-    color: rgb(255, 255, 255);
-    background-color: #f5f5f5;
-    color: rgba(0, 0, 0, 0.87);
+	margin: 0;
+	font-family: 'Montserrat', sans-serif;
+	color: rgb(255, 255, 255);
+	background-color: #f5f5f5;
+	color: rgba(0, 0, 0, 0.87);
 }
 h1 {
-    margin: 2rem 0;
-    text-transform: uppercase;
+	margin: 2rem 0;
+	text-transform: uppercase;
 }
 *,
 *::after,
 *::before {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
 }
 
 button {
-    font-family: 'Montserrat', sans-serif;
+	font-family: 'Montserrat', sans-serif;
 }
 a {
-    text-decoration: none;
-    &:focus {
-        outline: none;
-    }
+	text-decoration: none;
+	&:focus {
+		outline: none;
+	}
 }
 input::placeholder {
-    color: rgb(255, 255, 255);
+	color: rgb(255, 255, 255);
 }
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -9,78 +9,76 @@ const isLoading = ref(false);
 const successful = ref(false);
 
 async function register() {
-    isLoading.value = true;
-    try {
-        await createUserWithEmailAndPassword(
-            getAuth(),
-            email.value,
-            password.value
-        );
+	isLoading.value = true;
+	try {
+		await createUserWithEmailAndPassword(
+			getAuth(),
+			email.value,
+			password.value
+		);
 
-        isLoading.value = false;
-        successful.value = true;
+		isLoading.value = false;
+		successful.value = true;
 
-        setTimeout(() => {
-            router.push('/feed');
-        }, 500);
-    } catch (error) {
-        isLoading.value = false;
-        console.log(error.code);
-        alert(error.message);
-    }
+		router.push('/homework');
+	} catch (error) {
+		isLoading.value = false;
+		console.log(error.code);
+		alert(error.message);
+	}
 }
 </script>
 
 <template>
-    <section>
-        <h1>Create an Account</h1>
-        <form @submit.prevent class="register-form">
-            <v-text-field
-                v-model="email"
-                variant="underlined"
-                density="default"
-                label="Email"
-                placeholder="example@example.com"
-            />
-            <v-text-field
-                v-model="password"
-                variant="underlined"
-                density="default"
-                label="Password"
-                type="password"
-            />
-            <v-btn color="info" @click="register" type="submit">
-                <v-progress-circular
-                    v-if="isLoading"
-                    width="3"
-                    :size="25"
-                    indeterminate
-                    class="loading"
-                />
-                Submit
-            </v-btn>
-        </form>
-    </section>
+	<section>
+		<h1>Create an Account</h1>
+		<form @submit.prevent class="register-form">
+			<v-text-field
+				v-model="email"
+				variant="underlined"
+				density="default"
+				label="Email"
+				placeholder="example@example.com"
+			/>
+			<v-text-field
+				v-model="password"
+				variant="underlined"
+				density="default"
+				label="Password"
+				type="password"
+			/>
+			<v-btn color="info" @click="register" type="submit">
+				<v-progress-circular
+					v-if="isLoading"
+					width="3"
+					:size="25"
+					indeterminate
+					class="loading"
+				/>
+				Submit
+			</v-btn>
+		</form>
+	</section>
 </template>
 
 <style lang="scss" scoped>
 section {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 
-    .register-form {
-        min-width: 400px;
-        padding: 1rem;
-        display: flex;
-        flex-direction: column;
-        button {
-            margin-top: 1rem;
-            .loading {
-                margin-left: -16px;
-                margin-right: 12px;
-            }
-        }
-    }
+	.register-form {
+		min-width: 400px;
+		padding: 1rem;
+		display: flex;
+		flex-direction: column;
+		button {
+			margin-top: 1rem;
+			.loading {
+				margin-left: -16px;
+				margin-right: 12px;
+			}
+		}
+	}
 }
 </style>
