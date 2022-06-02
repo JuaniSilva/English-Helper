494b0e0ca6e9f002bfdfab8d21a1ab8f95f125f0
JuaniSilva <juanicruzsilva@gmail.com>
2022-06-01 22:18:06 -0300

    feat: initiating payments view

---
+++ src/views/Payments.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Button from '@/components/Button.vue';
+</script>
+<template>
+	<section>
+		<header>
+			<h1>Payments</h1>
+			<h4>Fee: $4000 per month</h4>
+		</header>
+		<div>
+			<Button> test </Button>
+		</div>
+	</section>
+</template>
+
+<style lang="scss" scoped>
+section {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	header {
+		text-align: center;
+		h4 {
+			color: grey;
+			font-weight: 400;
+		}
+	}
+}
+</style>
