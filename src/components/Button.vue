4a77614409dbdd31c2bbb184e8e17dbffddb2027
JuaniSilva <juanicruzsilva@gmail.com>
2022-06-01 22:17:21 -0300

    feat: initial button component

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineProps } from 'vue';
+import Loading from '@/components/Loading.vue';
+
+const props = defineProps({
+	isLoading: {
+		type: Boolean,
+		default: false
+	}
+});
+</script>
+<template>
+	<button>
+		<Loading v-if="isLoading" />
+		<slot></slot>
+	</button>
+</template>
+
+<style lang="scss" scoped></style>
